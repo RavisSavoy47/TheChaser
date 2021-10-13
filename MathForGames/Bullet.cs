23b6475deee3c8c2a4b9b24401581a38c2b57211
ravis <ravissavoy49@gmail.com>
2021-10-13 15:51:32 -0500

    Added a bullet class

---
+++ MathForGames/Bullet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MathLibrary1;
+
+namespace MathForGames
+{
+    class Bullet : Actor
+    {
+        private float _speed;
+        private Vector2 _velocity;
+
+        public float Speed
+        {
+            get { return _speed; }
+            set { _speed = value; }
+        }
+
+        public Vector2 Velocity
+        {
+            get { return _velocity; }
+            set { _velocity = value; }
+        }
+
+        public Bullet(char icon, float x, float y, float speed, string name = "Player", ConsoleColor color = ConsoleColor.White)
+            : base(icon, x, y, name, color)
+        {
+            _speed = speed;
+        }
+    }
+}
