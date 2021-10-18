141ca70dd3ef2c8238b591ca74e620af28967da6
ravis <ravissavoy49@gmail.com>
2021-10-18 09:46:35 -0500

    Edited the games window width

--- MathForGames/Engine.cs
+++ MathForGames/Engine.cs
@@ -153,7 +153,7 @@ namespace MathForGames
         private void Draw()
         {
             //Clear the stuff that was on the screen in the last frame
-            _buffer = new Icon[Console.WindowWidth, Console.WindowHeight - 1];
+            _buffer = new Icon[Console.WindowWidth - 1, Console.WindowHeight - 1];
 
             //Reset the cursor position to the top so the previous screen is drawn over
             Console.SetCursorPosition(0, 0);
